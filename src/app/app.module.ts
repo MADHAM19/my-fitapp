8737487cb51ad3db8f2de8276ea34e79732c74a6
madh <mad_cerber@mail.ru>
2018-04-23 18:22:58 +0600

    Adding service, in-memory-data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+import { HttpClientModule }    from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
+import { AbbrListComponent } from './abbr-list/abbr-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AbbrListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/exercise.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by madh on 23.04.2018.
+ */
+
+export class Exercise {
+  id: number;
+  Abbr_en: string;
+  Short_name_en: string;
+  Description_en: string;
+  Description_ru: string;
+  Img_id: string;
+}
